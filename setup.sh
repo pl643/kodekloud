09c1ff2cf61794ce8b1cc1eeb923b79298d4a02c
pl643 <peter.wt.ly@gmail.com>
2022-06-23 12:25:39 -0700

    removed starting of tmux to paste string

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # Setup KodeKloud Lab environment for tmux environment and sshpass to auto enter password
 # paste below command without the # to execute: 
-#    curl -s https://raw.githubusercontent.com/pl643/kodekloud/main/setup.sh | bash
+#    curl -s https://raw.githubusercontent.com/pl643/kodekloud/main/setup.sh | bash; tmux
 
 curl -s https://raw.githubusercontent.com/pl643/kodekloud/main/bashrc.kk > ~/bashrc.kk
 
@@ -9,4 +9,3 @@ grep bashrc.kk ~/.bashrc || echo "source ~/bashrc.kk" >> ~/.bashrc
 
 # install and execute tmux
 echo mjolnir123 | sudo -S yum install tmux sshpass
-tmux
