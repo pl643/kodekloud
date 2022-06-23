242b939cd627f7592363340f0addcf793de756eb
pl643 <peter.wt.ly@gmail.com>
2022-06-23 14:40:29 -0700

    create .ssh folder and copy over keys

--- setup.sh
+++ setup.sh
@@ -17,6 +17,8 @@ yum -y install tmux sshpass neovim
 [ ! -f ~/.ssh/id_rsa ] && ssh-keygen -q -N "" -f ~/.ssh/id_rsa
 
 # copy sshkey to systems for passwordless login
+sshpass -p Ir0nM@n ssh tony@stapp01 '[ -d .ssh ] || mkdir .ssh ]'
 sshpass -p Ir0nM@n ssh-copy-id tony@stapp01
-sshpass -p Am3ric@ ssh-copy-id steve@stapp02
 
+sshpass -p Am3ric@ ssh steve@stapp02 '[ -d .ssh ] || mkdir .ssh ]'
+sshpass -p Am3ric@ ssh-copy-id steve@stapp02 
