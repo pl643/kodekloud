3e5da8eb1da1a3fe8b0106238a09999f05634003
pl643 <peter.wt.ly@gmail.com>
2022-06-23 18:59:51 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,8 +20,8 @@ sudo yum -y install tmux sshpass neovim
 alias ssh='ssh -o userknownhostsfile=/dev/null -o StrictHostKeyChecking=no'
 
 # copy sshkey to systems for passwordless login
-sshpass -p Ir0nM@n ssh tony@stapp01 '[ -d .ssh ] || mkdir .ssh'
+sshpass -p Ir0nM@n ssh tony@stapp01 '[ -d .ssh ] || mkdir .ssh; chmod 700 .ssh'
 sshpass -p Ir0nM@n ssh-copy-id tony@stapp01
 
-sshpass -p Am3ric@ ssh steve@stapp02 '[ -d .ssh ] || mkdir .ssh'
+sshpass -p Am3ric@ ssh steve@stapp02 '[ -d .ssh ] || mkdir .ssh; chmod 700 .ssh'
 sshpass -p Am3ric@ ssh-copy-id steve@stapp02 
