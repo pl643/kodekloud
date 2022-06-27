6aa43bcddf09c98258f3ce880a3745733429cd66
pl643 <peter.wt.ly@gmail.com>
2022-06-27 16:23:36 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,43 @@
-# Setup KodeKloud Lab environment for tmux environment and sshpass to auto enter password
-# paste below command without the # to execute: 
+# Setup KodeKloud Lab environment for tmux environment ssh keys for less passord typing.
+ and sshpass to auto enter password
+# paste below command without the # to execute:
 #    curl -s https://raw.githubusercontent.com/pl643/kodekloud/main/setup.sh > setup.sh; bash -x setup.sh; tmux
 
-curl -s https://raw.githubusercontent.com/pl643/kodekloud/main/bashrc.kk > ~/bashrc.kk
+# retreive ~/bashrc.kk
+[ -f ~/bashrc.kk ] || curl -s https://raw.githubusercontent.com/pl643/kodekloud/main/bashrc.kk > ~/bashrc.kk
 
 # append sourcing of bashrc.kk to ~/.bashrc
 grep bashrc.kk ~/.bashrc || echo "source ~/bashrc.kk" >> ~/.bashrc
 
-# add thor to sudoer files
-echo mjolnir123 | sudo -S sh -c 'echo "thor ALL = NOPASSWD : ALL" >> /etc/sudoers'
+# add user to sudoer files
+passwordfile="passwords"
+[ -f $passwordfile ] || curl -s https://raw.githubusercontent.com/pl643/kodekloud/main/passwords > $passwordfile
+hostname=$(hostname | cut -f 1 -d.)
+user=$(grep $hostname $passwordfile | awk {'print $4'})
+password=$(grep $hostname $passwordfile | awk {'print $5'})
+# echo mjolnir123 | sudo -S sh -c 'echo "thor ALL = NOPASSWD : ALL" >> /etc/sudoers2'
+# echo $password | sudo -S sh   -c echo "$user ALL = NOPASSWD : ALL >> /etc/sudoers2"
+echo $password | sudo -S bash -c "echo $user ALL = NOPASSWD : ALL >> /etc/sudoers"
 
-# install tmux, sshpass
-sudo yum -y install tmux sshpass neovim
-
-# create sshkey
+# create default sshkey if doesn't exist
 [ ! -f ~/.ssh/id_rsa ] && ssh-keygen -q -N "" -f ~/.ssh/id_rsa
 
+# ssh options to prevent prompting
 SSHOPT="-o userknownhostsfile=/dev/null -o StrictHostKeyChecking=no"
 
-# copy sshkey to systems for passwordless login
-sshpass -p Ir0nM@n ssh $SSHOPT tony@stapp01 '[ -d .ssh ] || mkdir .ssh && chmod 700 .ssh'
-sshpass -p Ir0nM@n ssh-copy-id $SSHOPT tony@stapp01
+# jump(thor) system specific specific
+if [ "$hostname" = "thor" ]; then
+    # install tmux, sshpass
+    sudo yum -y install tmux sshpass neovim
 
-sshpass -p Am3ric@ ssh $SSHOPT steve@stapp02 '[ -d .ssh ] || mkdir .ssh && chmod 700 .ssh'
-sshpass -p Am3ric@ ssh-copy-id $SSHOPT steve@stapp02
+    passwordfile="passwords"
+    grep -v jump_host $passwordfile | while read line; do
+        hostname=$(echo $line | awk {'print $1'})
+        user=$(echo $line |  awk {'print $4'})
+        password=$(echo $line |  awk {'print $5'})
+
+        # copy sshkey to systems for passwordless login
+        sshpass -p $password ssh $SSHOPT $user@$hostname '[ -d .ssh ] || mkdir .ssh && chmod 700 .ssh'
+        sshpass -p $password ssh-copy-id $SSHOPT $user@$hostname
+    done
+fi
