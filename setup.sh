3a52dc0c4ab5409ebb438f99c649aa860cdf16ba
pl643 <peter.wt.ly@gmail.com>
2022-06-29 19:05:00 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-# Setup KodeKloud Lab environment for tmux and ssh keys for less passord typing.
+# Setup KodeKloud Lab environment for tmux and ssh keys for passwordless.
 # Paste below line without the # to execute:
 #    curl -Os https://raw.githubusercontent.com/pl643/kodekloud/main/setup.sh && bash -x setup.sh && tmux -f ~/tmux.conf
 
@@ -8,13 +8,12 @@
 # append sourcing of bashrc.kk to ~/.bashrc
 grep bashrc.kk ~/.bashrc || echo "source ~/bashrc.kk" >> ~/.bashrc
 
-# add user to sudoer files
+# add user to /etc/sudoers files to not prompt for password
 passwordfile="passwords"
 [ -f $passwordfile ] || curl -Os https://raw.githubusercontent.com/pl643/kodekloud/main/passwords
 hostname=$(hostname | cut -f 1 -d.)
 user=$(grep $hostname $passwordfile | awk {'print $4'})
 password=$(grep $hostname $passwordfile | awk {'print $5'})
-# echo mjolnir123 | sudo -S sh -c 'echo "thor ALL = NOPASSWD : ALL" >> /etc/sudoers2'
 echo $password | sudo -S bash -c "echo $user ALL = NOPASSWD : ALL >> /etc/sudoers"
 
 # create default sshkey if doesn't exist
@@ -28,12 +27,12 @@ if [ "$hostname" = "jump_host" ]; then
     # install tmux, sshpass, neovim
     [ -f yum.install.log ] || sudo yum -y install tmux sshpass 2>&1 > yum.install.log
     passwordfile="passwords"
-    grep app $passwordfile | while read line; do
+    grep stapp $passwordfile | while read line; do
         hostname=$(echo $line | awk {'print $1'})
         user=$(echo $line |  awk {'print $4'})
         password=$(echo $line |  awk {'print $5'})
 
-        # copy sshkey to app systems for passwordless login
+        # copy sshkey to stapp0x systems for passwordless login
         sshpass -p $password ssh-copy-id $SSHOPT $user@$hostname
     done
 fi
